9236db594335c3819eba5cdd4572f0206cb27ff8
Aleksa <bonixsrbija@gmail.com>
2025-03-17 16:39:51 +0100

    meteor star

---
+++ Content/Projectiles/Hostile/MeteorHostile.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrorMod.Content.Projectiles.Hostile
+{
+    public class MeteorHostile : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.Meteor1;
+
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.Meteor1);
+            AIType = ProjectileID.Meteor1;
+            Projectile.aiStyle = ProjAIStyleID.Arrow;
+            Projectile.hostile = true;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            SoundEngine.PlaySound(SoundID.Item89);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<BombExplosion>(), 20, 1);
+            }
+            return true;
+        }
+    }
+}
---
+++ Core/Globals/Projectiles/Hostile/FallenStar.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using TerrorMod.Content.NPCs.Hostile.Forest;
+using TerrorMod.Common.Utils;
+using TerrorMod.Content.Projectiles.Hostile;
+
+namespace TerrorMod.Core.Globals.Projectiles.Hostile
+{
+    public class FallenStar : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
+        {
+            return entity.type == ProjectileID.FallingStar;
+        }
+
+        public override void OnSpawn(Projectile projectile, IEntitySource source)
+        {
+            if (Main.rand.NextBool(50) && Main.hardMode)
+            {
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    Projectile.NewProjectile(projectile.GetSource_FromAI(), projectile.Center, projectile.velocity, ModContent.ProjectileType<MeteorHostile>(), 20, 1f, ai1: 1);
+                }
+                projectile.Kill();
+            }
+        }
+    }
+}
